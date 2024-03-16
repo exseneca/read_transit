c49589fba370e8e590e5fe07db890869e6fcc096
Sam D <sam-davidson1909@hotmail.co.uk>
2024-03-16 17:20:17 +0000

    release

--- resources/public/js/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { view_transit.home.init(); } catch (e) { console.error("An error occurred when calling (view-transit.home/init)"); throw(e); }
